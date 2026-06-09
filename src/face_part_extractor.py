a1cf382bbe6c408cf97b5a39be8f0687fc1ce955
yasmeen asaad <46120393+yasmeen-asaad@users.noreply.github.com>
2026-06-09 08:47:58 +0300

    Create face_part_extractor.py

---
+++ src/face_part_extractor.py
@@ -0,0 +1,25 @@
+class FacePartExtractor:
+
+    def extract_forehead(self):
+        pass
+
+    def extract_eyes(self):
+        pass
+
+    def extract_eyebrows(self):
+        pass
+
+    def extract_nose(self):
+        pass
+
+    def extract_mouth(self):
+        pass
+
+    def extract_ears(self):
+        pass
+
+    def extract_jaw_chin(self):
+        pass
+
+    def extract_whole_face(self):
+        pass

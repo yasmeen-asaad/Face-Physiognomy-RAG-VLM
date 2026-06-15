f6aa0ddf3daf84807cb9a72c882722eb27953f7a
yasmeen asaad <46120393+yasmeen-asaad@users.noreply.github.com>
2026-06-16 01:06:12 +0300

    Create face_descriper.py

---
+++ src/face_descriper.py
@@ -0,0 +1,25 @@
+class FaceDescriptor:
+
+    def describe_nose(self, image_path):
+        pass
+
+    def describe_eyes(self, image_path):
+        pass
+
+    def describe_eyebrows(self, image_path):
+        pass
+
+    def describe_forehead(self, image_path):
+        pass
+
+    def describe_mouth(self, image_path):
+        pass
+
+    def describe_ears(self, image_path):
+        pass
+
+    def describe_jaw_chin(self, image_path):
+        pass
+
+    def describe_whole_face(self, image_path):
+        pass

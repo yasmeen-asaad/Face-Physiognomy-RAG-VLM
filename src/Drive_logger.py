4305847fbc9997e0082687406c0e0592419e88e7
yasmeen asaad <46120393+yasmeen-asaad@users.noreply.github.com>
2026-06-19 04:26:56 +0300

    Update Drive_logger.py

--- src/Drive_logger.py
+++ src/Drive_logger.py
@@ -21,3 +21,38 @@ import numpy as np
 #____________________________________________
 @dataclass
 class SessionLog:
+    session_id : str
+    timestamp : str
+    email : str
+    status : str # success/ invalid_face/error
+    latency_sec : float
+    face_bbox : str    = ""
+    regions_ok : int    = 0 # how many face parts were successfully extracted
+    report_preview : str    = "" # report 
+    error_msg : str    = "" # if something failed
+
+#______________*______________*______________
+# Google drive logger class
+#____________________________________________
+class DriveLogger:
+    LOG_FILENAME = "log.csv"
+    CSV_COLUMNS = ["session_id", "timestamp", "email", "status",
+                   "latency_sec", "face_bbox", "regions_ok",
+                   "report_preview", "error_msg"]
+    def __init__(self):
+        pass
+	    
+    def _build_service(self, sa_json_str: str): 
+        pass
+	    
+    def log_session():
+        pass
+	    
+    def _create_folder():
+        pass
+	    
+    def _upload_text():
+        pass
+	    
+    def _append_to_csv():
+        pass
